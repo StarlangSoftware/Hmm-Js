e8e044f4be15e9f9f380df995755d8e2d6d99f7d
olcaytaner <olcay.yildiz@ozyegin.edu.tr>
2026-02-15 21:42:30 +0300

    Upgraded to nodeNext module.

--- dist/HmmState.js
+++ dist/HmmState.js
@@ -1,50 +1,42 @@
-(function (factory) {
-    if (typeof module === "object" && typeof module.exports === "object") {
-        var v = factory(require, exports);
-        if (v !== undefined) module.exports = v;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.HmmState = void 0;
+class HmmState {
+    emissionProbabilities;
+    state;
+    /**
+     * A constructor of {@link HmmState} class which takes a state and emission probabilities as inputs and
+     * initializes corresponding class variable with these inputs.
+     *
+     * @param state Data for this state.
+     * @param emissionProbabilities Emission probabilities for this state
+     */
+    constructor(state, emissionProbabilities) {
+        this.state = state;
+        this.emissionProbabilities = emissionProbabilities;
     }
-    else if (typeof define === "function" && define.amd) {
-        define(["require", "exports"], factory);
+    /**
+     * Accessor method for the state variable.
+     *
+     * @return state variable.
+     */
+    getState() {
+        return this.state;
     }
-})(function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    exports.HmmState = void 0;
-    class HmmState {
-        /**
-         * A constructor of {@link HmmState} class which takes a state and emission probabilities as inputs and
-         * initializes corresponding class variable with these inputs.
-         *
-         * @param state Data for this state.
-         * @param emissionProbabilities Emission probabilities for this state
-         */
-        constructor(state, emissionProbabilities) {
-            this.state = state;
-            this.emissionProbabilities = emissionProbabilities;
+    /**
+     * getEmitProb method returns the emission probability for a specific symbol.
+     *
+     * @param symbol Symbol for which the emission probability will be get.
+     * @return Emission probability for a specific symbol.
+     */
+    getEmitProb(symbol) {
+        if (this.emissionProbabilities.has(symbol)) {
+            return this.emissionProbabilities.get(symbol);
         }
-        /**
-         * Accessor method for the state variable.
-         *
-         * @return state variable.
-         */
-        getState() {
-            return this.state;
-        }
-        /**
-         * getEmitProb method returns the emission probability for a specific symbol.
-         *
-         * @param symbol Symbol for which the emission probability will be get.
-         * @return Emission probability for a specific symbol.
-         */
-        getEmitProb(symbol) {
-            if (this.emissionProbabilities.has(symbol)) {
-                return this.emissionProbabilities.get(symbol);
-            }
-            else {
-                return 0.0;
-            }
+        else {
+            return 0.0;
         }
     }
-    exports.HmmState = HmmState;
-});
+}
+exports.HmmState = HmmState;
 //# sourceMappingURL=HmmState.js.map
